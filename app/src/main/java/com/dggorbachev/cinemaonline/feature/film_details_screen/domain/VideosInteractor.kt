a44b49a4577899d87aa760b3dbc9350212c7041e
dggorbachev <dggorbachev@edu.hse.ru>
2022-05-19 15:21:31 +0300

    fix access in VideosInteractor.kt

--- app/src/main/java/com/dggorbachev/cinemaonline/feature/film_details_screen/domain/VideosInteractor.kt
+++ app/src/main/java/com/dggorbachev/cinemaonline/feature/film_details_screen/domain/VideosInteractor.kt
@@ -3,7 +3,7 @@ package com.dggorbachev.cinemaonline.feature.film_details_screen.domain
 import com.dggorbachev.cinemaonline.base.attempt
 import com.dggorbachev.cinemaonline.feature.film_details_screen.data.api.VideosRepo
 
-class VideosInteractor(val repo: VideosRepo) {
+class VideosInteractor(private val repo: VideosRepo) {
     suspend fun getVideosList(movieId: Int) = attempt {
         repo.getVideosList(movieId).filter {
             it.type == "Trailer"
